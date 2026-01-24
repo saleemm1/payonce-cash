b316431e5e183a7b9fbef503e291f41564eebf63
saleemm1 <168358635+saleemm1@users.noreply.github.com>
2026-01-25 01:31:43 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,15 +12,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "PayOnce Cash ",
+  title: "PayOnce Cash",
   description: "The non-custodial gateway to sell digital goods using Bitcoin Cash.",
   icons: {
-    icon: [
-      { url: '/logo.png', href: '/logo.png' }, 
-    ],
-    apple: [
-      { url: '/logo.png' },
-    ],
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
   },
 };
 
@@ -28,8 +25,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
-        <link rel="icon" href="/logo.png" />
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
