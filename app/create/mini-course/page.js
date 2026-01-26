0b4265c1350f57c47587579369e56bbbd5b0f147
saleemm1 <168358635+saleemm1@users.noreply.github.com>
2026-01-27 00:36:16 +0300

    Update page.js

--- app/create/mini-course/page.js
+++ app/create/mini-course/page.js
@@ -81,7 +81,7 @@ export default function CourseUploadPage() {
         </div>
 
         <div className="p-3 bg-zinc-800/30 rounded-lg border border-white/5">
-          <label className="text-[10px] text-zinc-400 mb-2 block uppercase text-center font-black">Intro Video or Image Preview</label>
+          <label className="text-[10px] text-zinc-400 mb-2 block uppercase text-center font-black">Intro Video or Image Preview (Optional)</label>
           <input type="file" accept="video/*,image/*" onChange={(e)=>setPreviewFile(e.target.files[0])} className="w-full text-xs text-zinc-500 mb-2 file:bg-zinc-700 cursor-pointer" />
           <input type="url" placeholder="Or Preview URL" value={previewLink} onChange={(e)=>setPreviewLink(e.target.value)} className="w-full p-2 bg-zinc-900 border border-zinc-700 rounded-lg text-xs outline-none focus:border-green-500" />
         </div>
